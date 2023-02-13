f678f1825cf2fd075fce98d3c4b20fdaf065b403
Evin <evinwoon@gmail.com>
2023-02-13 16:45:46 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let goldAmount = document.querySelector("#totalGold");
+
+gold = 0;
+wood = 0;
+
+function totalGold() {
+  goldAmount.textContent = gold;
+  if (gold<10){
+document.createElement("peasant")
+  }
+}
+
+function earn1Gold() {
+  gold++;
+  totalGold()
+}
+
+function buyPeasant({
+
+})
